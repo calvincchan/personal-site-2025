76070fbfb21c4d647d2364c2f2d3bb768b6ae9c0
Calvin C Chan <hello@calvin-c.com>
2025-04-28 09:58:17 -0700

    feat: add initial components for Nextra theme including Navbar, Sidebar, Footer, and TOC

---
+++ next.config.mjs
@@ -0,0 +1,23 @@
+import nextra from "nextra";
+
+const withNextra = nextra({
+  // theme: "nextra-theme-blog",
+  // themeConfig: "./theme.config.jsx",
+  defaultShowCopyCode: true,
+  readingTime: true,
+});
+
+export default withNextra({
+  reactStrictMode: true,
+  cleanDistDir: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "calvincchan.com",
+        // port: '',
+        // pathname: '/account123/**',
+      },
+    ],
+  },
+});
