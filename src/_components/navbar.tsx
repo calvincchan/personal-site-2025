4a6f7aedabf7e264745151a108ea8d729482362f
Calvin C Chan <hello@calvin-c.com>
2025-05-03 15:49:40 -0700

    fix: enhance Navbar component with CTA button and improve styling

--- src/_components/navbar.tsx
+++ src/_components/navbar.tsx
@@ -4,21 +4,21 @@ import { Anchor } from 'nextra/components';
 import type { FC } from 'react';
 
 export const Navbar: FC = () => {
-  const topLevelNavbarItems: { title: string, route: string; }[] = [
+  const topLevelNavbarItems: { title: string, route: string, cta?: boolean; }[] = [
     { title: 'Work', route: '/work' },
     { title: 'Blog', route: '/blog' },
-    { title: 'Contact', route: '/contact' }
+    { title: 'Contact', route: '/contact', cta: true },
   ];
 
   return (
-    <nav role="menu" className="x:px-2 x:py-5 x:flex x:items-center x:justify-between x:flex-col x:sm:flex-row">
-      <Anchor role="menuitem" href="/" className="x:font-medium x:hover:underline" rel="author">Calvin C. Chan</Anchor>
-      <ul role="menu" className="x:flex x:flex-row x:list-none x:gap-4 x:m-0">
+    <nav role="menu" className="x-top-nav">
+      <Anchor role="menuitem" href="/" className="x-main-link" rel="author">Calvin C. Chan</Anchor>
+      <ul role="menu" className="x-menu">
         {topLevelNavbarItems.map(item => {
           const route = item.route;
           return (
             <li key={route}>
-              <Anchor href={route} role="menuitem" className="x:hover:underline">
+              <Anchor href={route} role="menuitem" className={item.cta ? 'x-cta-button' : ''}>
                 {item.title}
               </Anchor>
             </li>
