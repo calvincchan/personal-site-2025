4b4c78490632c44d605fcd6553a9d47231bb8f65
Calvin C Chan <hello@calvin-c.com>
2025-05-03 15:33:38 -0700

    fix: update metadata for seo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,7 +12,55 @@ export const metadata: Metadata = {
     default: 'Freelance Web App and AI Developer | Calvin C. Chan',
     template: '%s | Calvin C. Chan'
   },
-  description: 'Freelance Web App and AI Developer ',
+  description: process.env.SITE_DESCRIPTION,
+  authors: [
+    {
+      name: process.env.SITE_AUTHOR,
+      url: process.env.SITE_URL,
+    },
+  ],
+  creator: process.env.SITE_AUTHOR,
+  keywords: [
+    'Calvin C. Chan',
+    'Freelance Developer',
+    'Web Developer',
+    'AI Developer',
+    'Software Engineer',
+    'Freelance Web Developer',
+    'Freelance Software Engineer',
+    'Freelance AI Developer',
+    'Freelance Software Developer',
+    'Freelance Web App Developer',
+    'Freelance Web App Engineer',
+    'Freelance Web App Designer',
+    'Freelance Web App Creator',
+    'Freelance Web App Builder',
+    'Freelance Web App Specialist',
+  ],
+  openGraph: {
+    title: process.env.SITE_AUTHOR,
+    description: process.env.SITE_DESCRIPTION,
+    url: process.env.SITE_URL,
+    siteName: process.env.SITE_AUTHOR,
+    type: 'website',
+    images: [
+      {
+        url: process.env.SITE_OG_IMAGE,
+        width: 640,
+        height: 640,
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: process.env.SITE_AUTHOR,
+    description: process.env.SITE_DESCRIPTION,
+    images: process.env.SITE_OG_IMAGE,
+    creator: process.env.SITE_AUTHOR,
+  },
+  icons: {
+    icon: '/favicon.ico',
+  }
 };
 
 export const viewport: Viewport = {
