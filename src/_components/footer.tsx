218ea09fdc121e370963012893314322af8be783
Calvin C Chan <hello@calvin-c.com>
2025-05-04 08:36:27 -0700

    fix: remove social media links from Footer component

--- src/_components/footer.tsx
+++ src/_components/footer.tsx
@@ -1,6 +1,5 @@
 import Link from "next/link";
 import type { FC } from 'react';
-import { GitHubIcon, LinkedInIcon, YouTubeIcon } from '../_icons';
 
 export const Footer: FC = () => {
   return (
@@ -15,15 +14,6 @@ export const Footer: FC = () => {
         </Link>
         <Link href="/contact">
           Contact
-        </Link>
-        <Link href="https://www.youtube.com/@calvincchan" title="YouTube">
-          <YouTubeIcon />
-        </Link>
-        <Link href="https://github.com/calvincchan" title="GitHub">
-          <GitHubIcon />
-        </Link>
-        <Link href="https://www.linkedin.com/in/calvincchan" title="LinkedIn">
-          <LinkedInIcon />
         </Link>
         <Link href="/feed.xml" title="Blog Post Feed">
           RSS
