ef736378ad7b5a35969484089922af4cab098359
Calvin C Chan <hello@calvin-c.com>
2025-05-27 11:13:24 -0700

    refactor: consolidate footer navigation links into a single array for improved maintainability

--- src/_components/footer.tsx
+++ src/_components/footer.tsx
@@ -3,6 +3,15 @@ import type { FC } from 'react';
 import { SiteLogoIcon } from "src/_icons";
 
 export const Footer: FC = () => {
+  const topLevelNavbarItems: { title: string, route: string, cta?: boolean; }[] = [
+    { title: 'Home', route: '/' },
+    { title: 'About', route: '/about' },
+    { title: 'Projects', route: '/projects' },
+    { title: 'Blog', route: '/blog' },
+    { title: 'Contact', route: '/contact', cta: true },
+    { title: 'RSS', route: '/feed.xml' }
+  ];
+
   return (
     <footer className="x-global-footer">
       <div className="x-row">
@@ -11,21 +20,11 @@ export const Footer: FC = () => {
           All rights reserved.
         </div>
         <div className="x-col2">
-          <Link href="/" title="Home">
-            Home
-          </Link>
-          <Link href="/blog" title="Blog">
-            Blog
-          </Link>
-          <Link href="/projects" title="Projects">
-            Projects
-          </Link>
-          <Link href="/contact">
-            Contact
-          </Link>
-          <Link href="/feed.xml" title="Blog Post Feed">
-            RSS
-          </Link>
+          {topLevelNavbarItems.map((item) => (
+            <Link key={item.route} href={item.route} title={item.title}>
+              {item.title}
+            </Link>
+          ))}
         </div>
       </div>
       <div className="x:flex x:justify-center x:mt-8 x:text-neutral-400">
