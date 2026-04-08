ce720a1280591f75dc3894d579f5e1a4d45ca96a
Calvin Chan <hello@calvin-c.com>
2026-04-08 11:34:14 -0700

    chore: update logo image

--- src/_icons/SiteLogoIcon.tsx
+++ src/_icons/SiteLogoIcon.tsx
@@ -5,7 +5,7 @@ import React from 'react';
 const SiteLogoIcon: React.FC<{ size: number; }> = ({ size = 100 }) => {
   return (
     <Link href="/" title="Home | Calvin C Chan">
-      <Image src="/logo.svg" alt="Calvin C Chan Logo" width={size} height={size} />
+      <Image src="/logo.png" alt="Calvin C Chan Logo" width={size} height={size} />
     </Link>
   );
 };
