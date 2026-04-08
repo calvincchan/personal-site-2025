f111d49e32b76b8af26b1e6e9f5616f5cae04075
Calvin Chan <hello@calvin-c.com>
2026-04-08 11:34:48 -0700

    chore: clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,12 @@
 // DEBUG: layout.tsx loaded from src/app
 import { siteConfig } from "@/lib/site-config";
 // import { Footer, Layout, Navbar, ThemeSwitch } from 'nextra-theme-blog'
-import { Metadata } from 'next';
+import { FrontMatter } from "nextra";
 import { getPageMap } from 'nextra/page-map';
 import { FC, ReactNode } from 'react';
 import { NextraTheme } from '../_components/nextra-theme';
-import '../globals.css';
 
-export const metadata: Metadata = {
+export const metadata: FrontMatter = {
   title: {
     default: `Freelance Web App and AI Developer | ${siteConfig.author}`,
     template: `%s | ${siteConfig.author}`
